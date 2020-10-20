d4290d6561722d3747e457946059cec18102d4bc
zeyadShapan <zeyadshapan2004@gmail.com>
2020-10-20 10:50:41 +0200

    Fixed not showing messages recorded by microphone

--- chat/admin.py
+++ chat/admin.py
@@ -1,12 +1,25 @@
 from django.contrib import admin
+from django.urls import resolve
 from .models import Room, Area, Message
 
 
 admin.site.register(Room)
 admin.site.register(Area)
+
+
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
     '''Admin View for Message'''
 
-    list_display = ('user','room', 'user', 'is_read', 'text')
-    readonly_fields = ('date',)
+    list_display = ('user', 'user', 'is_read', 'get_content',)
+    def get_content(self, obj):
+        if obj.text:
+            return obj.text
+        elif obj.video:
+            return obj.video.url
+        elif obj.image:
+            return obj.image.url
+        elif obj.file:
+            return obj.file.url
+        elif obj.audio:
+            return obj.audio.url
