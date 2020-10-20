742f70928c5cc7079abac4896329087ee5af147a
zeyadShapan <zeyadshapan2004@gmail.com>
2020-10-20 05:16:07 +0200

    Changed admin list display

--- chat/admin.py
+++ chat/admin.py
@@ -4,4 +4,9 @@ from .models import Room, Area, Message
 
 admin.site.register(Room)
 admin.site.register(Area)
-admin.site.register(Message)
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    '''Admin View for Message'''
+
+    list_display = ('user','room', 'user', 'is_read', 'text')
+    readonly_fields = ('date',)
