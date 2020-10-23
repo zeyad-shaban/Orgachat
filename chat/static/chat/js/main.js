9f2f9b159009cc15683e220dced8b28615577b51
zeyadShapan <zeyadshapan2004@gmail.com>
2020-10-23 14:48:52 +0200

    Fixed always showing install now beta

--- chat/static/chat/js/main.js
+++ chat/static/chat/js/main.js
@@ -3,36 +3,34 @@ document.addEventListener('DOMContentLoaded', function () {
     // Service worker 
     // --------------------------
     // -------Add to homepage--------
-    document.addEventListener("DOMContentLoaded", function () {
-        // --------Add to homepage----------
-        let deferredPrompt;
-        const addBtn = document.querySelector('#addToHomescreen');
-        addBtn.style.display = 'none';
-        window.addEventListener('beforeinstallprompt', (e) => {
-            // Prevent Chrome 67 and earlier from automatically showing the prompt
-            e.preventDefault();
-            // Stash the event so it can be triggered later.
-            deferredPrompt = e;
-            // Update UI to notify the user they can add to home screen
-            addBtn.style.display = 'block';
+    // --------Add to homepage----------
+    let deferredPrompt;
+    const addBtn = document.querySelector('#addToHomescreen');
+    addBtn.style.display = 'none';
+    window.addEventListener('beforeinstallprompt', (e) => {
+        // Prevent Chrome 67 and earlier from automatically showing the prompt
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        deferredPrompt = e;
+        // Update UI to notify the user they can add to home screen
+        addBtn.style.display = 'block';
 
-            addBtn.addEventListener('click', (e) => {
-                // hide our user interface that shows our A2HS button
-                // addBtn.style.display = 'none';
-                // Show the prompt
-                deferredPrompt.prompt();
-                // Wait for the user to respond to the prompt
-                deferredPrompt.userChoice.then((choiceResult) => {
-                    if (choiceResult.outcome === 'accepted') {
-                        console.log('User accepted the A2HS prompt');
-                        var xhr = new XMLHttpRequest()
-                        xhr.open("GET", "/metrics/user_installed/", true)
-                        xhr.send()
-                    } else {
-                        console.log('User dismissed the A2HS prompt');
-                    }
-                    deferredPrompt = null;
-                });
+        addBtn.addEventListener('click', (e) => {
+            // hide our user interface that shows our A2HS button
+            // addBtn.style.display = 'none';
+            // Show the prompt
+            deferredPrompt.prompt();
+            // Wait for the user to respond to the prompt
+            deferredPrompt.userChoice.then((choiceResult) => {
+                if (choiceResult.outcome === 'accepted') {
+                    console.log('User accepted the A2HS prompt');
+                    var xhr = new XMLHttpRequest()
+                    xhr.open("GET", "/metrics/user_installed/", true)
+                    xhr.send()
+                } else {
+                    console.log('User dismissed the A2HS prompt');
+                }
+                deferredPrompt = null;
             });
         });
     });
