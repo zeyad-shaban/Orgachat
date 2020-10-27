6aa565d7baca8c7730fac8e698e6ef7f420a3419
zeyadShapan <zeyadshapan2004@gmail.com>
2020-10-27 09:10:55 +0200

    Working PWA

--- chat/templates/chat/sw.js
+++ chat/templates/chat/sw.js
@@ -1,3 +1,58 @@
+// --------------------PWA--------------------
+self.addEventListener("install", function (event) {
+    event.waitUntil(preLoad());
+});
+
+var preLoad = function () {
+    console.log("Installing web app");
+    return caches.open("offline").then(function (cache) {
+        console.log("caching index and important routes");
+        return cache.addAll(["/users/", "/users/all", "/", "/users/about/",]);
+    });
+};
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(checkResponse(event.request).catch(function () {
+        return returnFromCache(event.request);
+    }));
+    event.waitUntil(addToCache(event.request));
+});
+
+var checkResponse = function (request) {
+    return new Promise(function (fulfill, reject) {
+        fetch(request).then(function (response) {
+            if (response.status !== 404) {
+                fulfill(response);
+            } else {
+                reject();
+            }
+        }, reject);
+    });
+};
+
+var addToCache = function (request) {
+    return caches.open("offline").then(function (cache) {
+        return fetch(request).then(function (response) {
+            console.log(response.url + " was cached");
+            return cache.put(request, response);
+        });
+    });
+};
+
+var returnFromCache = function (request) {
+    return caches.open("offline").then(function (cache) {
+        return cache.match(request).then(function (matching) {
+            if (!matching || matching.status == 404) {
+                return cache.match("offline.html");
+            } else {
+                return matching;
+            }
+        });
+    });
+};
+// --------------------END PWA--------------------
+
+// --------------------PUSH NOTIFICATIONS--------------------
 self.addEventListener('push', function (event) {
     console.log('[Service Worker] Push Received.');
     console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
@@ -21,3 +76,4 @@ self.addEventListener('notificationclick', function (event) {
         clients.openWindow('https://www.orgachat.com')
     );
 });
+// --------------------END PUSH NOTIFICATIONS--------------------
