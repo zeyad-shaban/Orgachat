e711cf44e175c8eeb3dbee6ca58fa6aa07e6fe34
zeyadShapan <zeyadshapan2004@gmail.com>
2020-11-21 09:15:28 +0200

    Fixed improper poining to orgachat settings in asgi.py

--- orgachat/asgi.py
+++ orgachat/asgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 import django
 from channels.routing import get_default_application
+from django.core.management import call_command
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orgachat.settings')
 django.setup()
 application = get_default_application()
